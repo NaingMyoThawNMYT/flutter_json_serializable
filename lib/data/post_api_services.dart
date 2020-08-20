7591f7e509b0c8ce4d459a7724ba5ae38b5267f5
NaingMyoThawNMYT <naingmyothaw.nmyt@gmail.com>
2020-08-20 07:35:16 +0630

    Post api is implemented.

--- lib/data/post_api_services.dart
+++ lib/data/post_api_services.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:flutter_json_serializable/models/post.dart';
 import 'package:http/http.dart' as http;
 
 const BASE_URL = 'https://jsonplaceholder.typicode.com/';
@@ -9,7 +12,14 @@ class PostAPIServices {
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
 
-    // TODO: handle response
+    if (response.statusCode == 200) {
+      final posts = List<Post>();
+      final data = jsonDecode(response.body);
+      for (Map i in data) {
+        posts.add(Post.fromJson(i));
+      }
+      return posts;
+    }
 
     return 'Failed to get posts!';
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_json_serializable/data/post_api_services.dart';
 
+import 'models/post.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -14,36 +16,68 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Json Serializable'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    PostAPIServices.getPosts();
-
-    super.initState();
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text('Flutter Json Serializable'),
+      appBar: appBar,
+      body: body,
+    );
+  }
+
+  AppBar get appBar => AppBar(title: Text('Flutter Json Serializable'));
+
+  Widget get body => FutureBuilder(
+        future: PostAPIServices.getPosts(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Center(child: Text(snapshot.error.toString()));
+            }
+
+            if (!snapshot.hasData) {
+              return Center(child: Text('Something wrong!'));
+            }
+
+            if (snapshot.data is String) {
+              return Center(child: Text(snapshot.data));
+            }
+
+            return buildPostListView(snapshot.data);
+          }
+
+          return Center(child: CircularProgressIndicator());
+        },
+      );
+
+  Widget buildPostListView(List<Post> posts) {
+    return ListView.separated(
+      physics: BouncingScrollPhysics(),
+      itemBuilder: (_, index) => postListTile(posts[index]),
+      separatorBuilder: (_, __) => SizedBox(height: 8),
+      itemCount: posts.length,
+    );
+  }
+
+  Widget postListTile(Post post) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      color: Colors.black12,
+      child: Column(
+        children: [
+          Text(
+            post.title,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 8),
+          Text(post.body, textAlign: TextAlign.start),
+        ],
       ),
     );
   }
